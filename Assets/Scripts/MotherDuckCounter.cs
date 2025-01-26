70a3149db3e08da90cf26dfce0d36e5be5d1fb54
JDawes <j.dawes@falmouth.ac.uk>
2025-01-26 14:10:50 +0000

    Added reference to singleton to count ducklings

--- Assets/Scripts/MotherDuckCounter.cs
+++ Assets/Scripts/MotherDuckCounter.cs
@@ -10,17 +10,21 @@ public class MotherDuckCounter : MonoBehaviour
     public int ducklingCount;
     
     public Vector3 ducklingDisUnit;
+    
+    private GameSingleton gameSingleton;
 
 
     private void Start()
     {
         Ducks.Add(transform);
+        gameSingleton = GameSingleton.GetInstance();
     }
 
     public void UpdateDucklingCount(Transform duck)
     {
         ducklingCount = ducklingCount + 1;
         Ducks.Add(duck);
+        gameSingleton.AddDuckling();
         //ducklingDisUnit = new Vector3(ducklingDisUnit.x, ducklingDisUnit.y, ducklingDisUnit.z * ducklingCount);
     }
 
